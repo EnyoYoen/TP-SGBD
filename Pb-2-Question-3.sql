f951aff4db4f2a309e49923cab6542c82ec29aaa
WiredMind2 <william@michaud.re>
2024-12-18 16:55:30 +0100

    q3

--- Pb-2-Question-3.sql
+++ Pb-2-Question-3.sql
@@ -4,14 +4,32 @@
 -- Membre 2 : 4028437		   Michaud	William
 
 -- Probleme 2 - Question 3 : Nom des lacs pour lesquels aucune des provinces sur lesquels ils sont situés n’a de montagne répertoriée. Classer les noms par ordre alphabétique.
-SELECT DISTINCT l.NAME
-    FROM Lake l, geo_lake g 
-    WHERE l.NAME = g.LAKE AND l.AREA >= 50000 AND (g.PROVINCE, g.COUNTRY) NOT IN (
-        SELECT name, country
-        FROM Province 
-        WHERE (name, country) NOT IN (
-            SELECT province, country
-            FROM geo_mountain
-        )
-    )
-    ORDER BY l.NAME ASC;
+select distinct l.name
+  from lake l,
+       geo_lake g
+ where l.name = g.lake
+   and l.area >= 50000
+   and ( g.province,
+         g.country ) not in (
+   select name,
+          country
+     from province
+    where ( name,
+            country ) not in (
+      select province,
+             country
+        from geo_mountain
+   )
+)
+ order by l.name asc;
+
+select name
+  from lake
+ where name not in (
+   select l.lake
+     from geo_lake l,
+          geo_mountain m
+    where l.province = m.province
+      and l.country = m.country
+)
+ order by name asc;
