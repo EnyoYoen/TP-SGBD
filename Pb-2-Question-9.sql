61f5bd0331569637bb2c523155dddf47ea8b8d00
WiredMind2 <william@michaud.re>
2024-12-18 17:50:48 +0100

    Comments pb2 q9

--- Pb-2-Question-9.sql
+++ Pb-2-Question-9.sql
@@ -5,6 +5,19 @@
 
 -- Probleme 2 - Question 9 : Donnez le nombre de pays ayant 0 lac, le nombre de pays ayant au moins un lac, mais moins de 10, nombre de pays ayant au moins 10 lacs mais moins de 100 et nombre de plays ayant plus de 100 lacs.
 
+select COUNT(case when lake_count = 0 then 1 end) as "Nombre de pays ayant 0 lac",
+       COUNT(case when lake_count > 0 and lake_count < 10 then 1 end) as "Nombre de pays ayant au moins un lac, mais moins de 10",
+       COUNT(case when lake_count >= 10 and lake_count < 100 then 1 end) as "Nombre de pays ayant au moins 10 lacs mais moins de 100",
+       COUNT(case when lake_count >= 100 then 1 end) as "Nombre de pays ayant plus de 100 lacs"
+  from ( -- selectionne le code de chaque pays et son nombre de lacs
+    select country.code, count(geo_lake.lake) as lake_count
+      from country
+      left join geo_lake -- au cas où il n'y a pas de lac dans le pays, il faut quand même renvoyer 0
+        on country.code = geo_lake.country
+     group by country.code
+  );
+
+
 /*
 -- Tests pour vérifier les résultats
 select name
@@ -35,16 +48,3 @@ having count(lake) >= 100;
 
 select COUNT(*) FROM COUNTRY;
 */
-
-select COUNT(case when lake_count = 0 then 1 end) as "Nombre de pays ayant 0 lac",
-       COUNT(case when lake_count > 0 and lake_count < 10 then 1 end) as "Nombre de pays ayant au moins un lac, mais moins de 10",
-       COUNT(case when lake_count >= 10 and lake_count < 100 then 1 end) as "Nombre de pays ayant au moins 10 lacs mais moins de 100",
-       COUNT(case when lake_count >= 100 then 1 end) as "Nombre de pays ayant plus de 100 lacs"
-  from (
-    select country.code,
-           count(geo_lake.lake) as lake_count
-      from country
-      left join geo_lake
-        on country.code = geo_lake.country
-     group by country.code
-  );
