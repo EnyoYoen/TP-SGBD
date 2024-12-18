e61797396304bd732d9a9ff86d7c79d14544e39d
EnyoYoen <enyotoyrep@gmail.com>
2024-12-18 16:07:53 +0100

    2.1 FIX

--- Pb-2-Question-1.sql
+++ Pb-2-Question-1.sql
@@ -2,7 +2,6 @@
 --            NumEtudiant      Nom      Prénom 
 -- Membre 1 : 04028297         Peyrot   Yoen
 -- Membre 2 : 
--- Membre 3 : 
 
 -- Probleme 2 - Question 1 : Nom des lacs qui sont situés sur 5 provinces ou plus. Afficher le nom du lac et le nombre de provinces, par ordre décroissant de ce nombre.
-SELECT name FROM Lake, geo_lake WHERE LAKE = NAME GROUP BY NAME HAVING COUNT(PROVINCE) >= 5 ORDER BY COUNT(PROVINCE) DESC;
+SELECT name, COUNT(PROVINCE) FROM Lake, geo_lake WHERE LAKE = NAME GROUP BY NAME HAVING COUNT(PROVINCE) >= 5 ORDER BY COUNT(PROVINCE) DESC;
