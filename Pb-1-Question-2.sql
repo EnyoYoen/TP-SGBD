4109df99988f457bf79654628267ad9426487403
WiredMind2 <william@michaud.re>
2024-12-18 16:30:51 +0100

    Partie 1

--- Pb-1-Question-2.sql
+++ Pb-1-Question-2.sql
@@ -1,10 +1,18 @@
 -- Contrôle de TP
 --            NumEtudiant      Nom      Prénom 
--- Membre 1 : 
--- Membre 2 : 
--- Membre 3 : 
+-- Membre 1 : 04028297         Peyrot   Yoen
+-- Membre 2 : 4028437			Michaud	William
+
 
 -- Probleme 1 - Question 2 : Donnez le code nécessaire pour que le nom d'un artiste soit toujours stocké en majuscules.
 
 ALTER TABLE Artiste
 ADD CONSTRAINT majuscule_nomArtiste CHECK (nomArtiste = UPPER(nomArtiste));
+
+-- Tests
+
+INSERT INTO Artiste VALUES (1, 'Dupont', 'Jean', '01-JAN-2000'); -- Erreur
+
+INSERT INTO Artiste VALUES (1, 'DUPONT', 'Jean', '01-JAN-2000'); -- OK
+
+COMMIT;
--- Pb-1-Question-4.sql
+++ Pb-1-Question-4.sql
@@ -1,7 +1,16 @@
 -- Contrôle de TP
 --            NumEtudiant      Nom      Prénom 
--- Membre 1 : 
--- Membre 2 : 
--- Membre 3 : 
+-- Membre 1 : 04028297         Peyrot   Yoen
+-- Membre 2 : 4028437			Michaud	William
+
 
 -- Probleme 1 - Question 4 : Donnez le code nécessaire pour que la capacité d'une salle soit supérieure à 30.
+
+ALTER TABLE Salle
+ADD CONSTRAINT capacite_sup CHECK (capacite > 30);
+
+-- Tests
+INSERT INTO Salle VALUES (1, 1, 30, 1); -- Erreur
+
+INSERT INTO Salle VALUES (1, 1, 31, 1); -- OK
+COMMIT;
