40eaf67889885ea85d27a584422e808ca67b879e
WiredMind2 <william@michaud.re>
2024-12-18 17:12:23 +0100

    pb2 ex3+ex8 v2

--- Pb-2-Question-3.sql
+++ Pb-2-Question-3.sql
@@ -4,24 +4,6 @@
 -- Membre 2 : 4028437		   Michaud	William
 
 -- Probleme 2 - Question 3 : Nom des lacs pour lesquels aucune des provinces sur lesquels ils sont situés n’a de montagne répertoriée. Classer les noms par ordre alphabétique.
-select distinct l.name
-  from lake l,
-       geo_lake g
- where l.name = g.lake
-   and l.area >= 50000
-   and ( g.province,
-         g.country ) not in (
-   select name,
-          country
-     from province
-    where ( name,
-            country ) not in (
-      select province,
-             country
-        from geo_mountain
-   )
-)
- order by l.name asc;
 
 select DISTINCT name
   from lake, geo_lake
@@ -34,6 +16,7 @@ select DISTINCT name
       and l.country = m.country
 )
  order by name asc;
+
 SELECT DISTINCT lake
     FROM geo_lake
     WHERE (province, country) NOT IN (
--- Pb-2-Question-8.sql
+++ Pb-2-Question-8.sql
@@ -4,7 +4,17 @@
 -- Membre 2 : 4028437		   Michaud	William
 
 -- Probleme 2 - Question 8 : Pour les pays de superficie supérieure ou égale à 8 millions de $km^{2
-SELECT DISTINCT code, lake
-    FROM Country c LEFT OUTER JOIN geo_lake g ON (c.code = g.country)
-    WHERE area >= 8000000
-    ORDER BY code ASC, lake ASC;
+select distinct code,
+                lake
+  from country c
+  left outer join geo_lake g
+on ( c.code = g.country )
+ where area >= 8000000
+ order by code asc,
+          lake asc;
+
+select distinct c.code, l.lake
+  from country c
+  left join geo_lake l on c.code = l.country
+ where area >= 8000000
+ order by c.code, l.lake;
