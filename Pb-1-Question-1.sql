593e4bcb96e62c98b5a7cd041e062dfce927c96e
WiredMind2 <william@michaud.re>
2024-12-18 16:08:19 +0100

    q1-2

--- Pb-1-Question-1.sql
+++ Pb-1-Question-1.sql
@@ -1,7 +1,57 @@
 -- Contrôle de TP
 --            NumEtudiant      Nom      Prénom 
 -- Membre 1 : 
--- Membre 2 : 
+-- Membre 2 : 4028437			Michaud	William
 -- Membre 3 : 
 
 -- Probleme 1 - Question 1 : Donnez le code permettant de créer les tables correspondantes sous Oracle.
+
+CREATE TABLE Cinema (
+	idCinema INT PRIMARY KEY,
+	nomCinema VARCHAR(50),
+	arrondissement INT,
+	address VARCHAR(100)
+);
+
+CREATE TABLE Artiste (
+	idArtiste INT PRIMARY KEY,
+	nomArtiste VARCHAR(50),
+	prenomArtiste VARCHAR(50),
+	dateNaissance DATE
+);
+
+CREATE TABLE Film (
+	idFilm INT PRIMARY KEY,
+	titre VARCHAR(50),
+	dateSortie DATE,
+	realisePar INT,
+	FOREIGN KEY (realisePar) REFERENCES Artiste(idArtiste)
+);
+
+CREATE TABLE Salle (
+	idSalle INT PRIMARY KEY,
+	numero INT,
+	capacite INT,
+	idCinema INT,
+	FOREIGN KEY (idCinema) REFERENCES Cinema(idCinema)
+);
+
+CREATE TABLE Projection (
+	idSalles INT,
+	idFilms INT,
+	dateProjection DATE,
+	heureDebut TIME,
+	heureFin TIME,
+	PRIMARY KEY (idSalles, idFilms),
+	FOREIGN KEY (idSalles) REFERENCES Salle(idSalle),
+	FOREIGN KEY (idFilms) REFERENCES Film(idFilm)
+); 
+
+CREATE TABLE Joue (
+	idArtiste INT,
+	idFilm INT,
+	nomRole VARCHAR(50),
+	PRIMARY KEY (idArtiste, idFilm),
+	FOREIGN KEY (idArtiste) REFERENCES Artiste(idArtiste),
+	FOREIGN KEY (idFilm) REFERENCES Film(idFilm)
+);
--- Pb-1-Question-2.sql
+++ Pb-1-Question-2.sql
@@ -5,3 +5,6 @@
 -- Membre 3 : 
 
 -- Probleme 1 - Question 2 : Donnez le code nécessaire pour que le nom d'un artiste soit toujours stocké en majuscules.
+
+ALTER TABLE Artiste
+ADD CONSTRAINT majuscule_nomArtiste CHECK (nomArtiste = UPPER(nomArtiste));
